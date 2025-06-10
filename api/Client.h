eee27b06b41462aa0125e6df7b553576296213ff
Andrea Gilardoni <a.gilardoni@arduino.cc>
2025-06-10 16:24:40 +0200

    Splitting the definition of client into 2 interfaces
    - ClientConnect provides the abstraction required to address connection
      methods for TCP like sessions.
    - Client provides the same Features as before, but it is specialized to
      handle connections that are then handled like a stream, like a
      classical TCP connection
    
    This distinction will be used to differentiate stream like communication
    protocols, i.e. websocket, tcp, http streaming, from discrete sized
    packets communication, like MQTT.

--- api/Client.h
+++ api/Client.h
@@ -24,23 +24,28 @@
 
 namespace arduino {
 
-class Client : public Stream {
-
+// The objective of this interface is to split the definition of a Client that
+// works with streams and one that works with discrete sized messages
+class ClientConnect {
 public:
-  virtual int connect(IPAddress ip, uint16_t port) =0;
-  virtual int connect(const char *host, uint16_t port) =0;
-  virtual size_t write(uint8_t) =0;
-  virtual size_t write(const uint8_t *buf, size_t size) =0;
-  virtual int available() = 0;
-  virtual int read() = 0;
-  virtual int read(uint8_t *buf, size_t size) = 0;
-  virtual int peek() = 0;
-  virtual void flush() = 0;
-  virtual void stop() = 0;
+  virtual ~ClientConnect() = default;
+
+  virtual int connect(IPAddress ip, uint16_t port) = 0;
+  virtual int connect(const char *host, uint16_t port) = 0;
+  virtual void disconnect() = 0;
+
   virtual uint8_t connected() = 0;
   virtual operator bool() = 0;
+};
+
+class Client : public Stream, ClientConnect {
+public:
+  virtual size_t write(uint8_t) = 0;
+  virtual size_t write(const uint8_t *buf, size_t size) = 0;
+  virtual int read(uint8_t *buf, size_t size) = 0;
+  virtual void stop() = 0;
 protected:
-  uint8_t* rawIPAddress(IPAddress& addr) { return addr.raw_address(); };
+  uint8_t* rawIPAddress(IPAddress& addr) { return addr.raw_address(); }; // FIXME this is a utility function
 };
 
 }
